d3970bf1c925b971cca81764d80f76e4b98d9ff4
Saleck Yessoufou <ysaleck607@gmail.com>
2023-06-03 11:24:50 -0400

    Initial Commit

---
+++ Devise-Authentification/src/test/java/com/example/devise/DeviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
